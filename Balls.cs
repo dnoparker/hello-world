50d4feb507c0e01b19f58152856c33037feab909
Deano <dnoparker@gmail.com>
2015-03-17 17:15:34 +0000

    no message

---
+++ Balls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Balls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col) // if the balls hit eachother
+	{
+		if(col.gameObject.tag == "Cylinder")
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+
+		if(col.gameObject.name == "Bound")
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
